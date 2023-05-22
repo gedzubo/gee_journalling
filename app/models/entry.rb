f02c85561499cb8de6e9d6f9af9bb94ae7a7da40
Gediminas Zubovicius <gediminas@bloomandwild.com>
2023-05-22 16:24:32 +0100

    Adding annotate profile and fixing some files

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,18 @@
 class Entry < ApplicationRecord
+  has_rich_text :content
 end
+
+# == Schema Information
+#
+# Table name: entries
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_entries_on_user_id  (user_id)
+#
